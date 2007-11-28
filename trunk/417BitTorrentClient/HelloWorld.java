cbcf1647a1b6f53607a2d5d718cec53d9500ab77
Will.I.Rall <Will.I.Rall@47b1fde3-503f-0410-ba13-9bbf17867248>
2007-11-28 07:24:11 +0000

    Fundamental changes to parsing messages received from peers - now done within Peer

--- trunk/417BitTorrentClient/HelloWorld.java
+++ trunk/417BitTorrentClient/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 		System.out.println("hello andrew");
 		
 		// Handshake Message Received:
-		byte[] bytes = (new String("" + ((char)19) + "BitTorrent protocol" + new String(new byte[]{0,0,0,0,0,0,0,0}) + new String(new byte[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,14,13,12,11,1}) + new String(new byte[]{15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0,1,2,3,4}))).getBytes();
+		byte[] bytes = (new String("" + ((char)19) + "BitTorrent protocol" + new String(new byte[]{0,0,0,0,0,0,0,0}) + new String(new byte[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,14,13,12,11,1}) + new String(new byte[]{15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0,1,2,3,4}) + new String(new byte[]{0,0,0,1,3}))).getBytes();
 		ByteBuffer b = ByteBuffer.allocate(100);
 		
 		b.put(bytes);
@@ -24,6 +24,22 @@ public class HelloWorld {
 		System.out.println(Peer.getBytesAsHex(external_info_hash));
 		System.out.println(Peer.getBytesAsHex(external_peer_id));
 		
+		b.position(68);
+		b.compact();
+		b.position(0);
+		System.out.println(b.remaining());
+		
+		System.out.println(b.position());
+		System.out.println(b.getInt(0));
+		System.out.println(b.get(4));
+		
 		System.out.println("" + (byte)b.get(0) + "       hello -" + b.array()[0] + " " + ByteBuffer.wrap(external_info_hash).equals(ByteBuffer.wrap(new byte[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,14,13,12,11,1})));
+		
+		System.out.println(works(null));
+	}
+	
+	public static boolean works(Object o)
+	{
+		return (o == null);
 	}
 }
