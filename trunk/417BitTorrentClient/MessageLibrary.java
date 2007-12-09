3531ac4d4092725dbcd5cf6ddd03502aa9e26b28
Will.I.Rall <Will.I.Rall@47b1fde3-503f-0410-ba13-9bbf17867248>
2007-12-09 00:56:55 +0000

    Made numerous additions to sendMessage and Peer.  This should make it mostly done, but it is untested and there are other things that must be done still (mostly in readAndParse to manage state and such).

--- trunk/417BitTorrentClient/MessageLibrary.java
+++ trunk/417BitTorrentClient/MessageLibrary.java
@@ -4,10 +4,10 @@ public class MessageLibrary
 {
 
 	public static final byte[] keep_alive = 		new byte[]{0,0,0,0};
-	public static final byte[] choke = 			new byte[]{0,0,0,1,0};
+	public static final byte[] choke = 				new byte[]{0,0,0,1,0};
 	public static final byte[] unchoke = 			new byte[]{0,0,0,1,1};
 	public static final byte[] interested = 		new byte[]{0,0,0,1,2};
-	public static final byte[] not_interested =	new byte[]{0,0,0,1,3};
+	public static final byte[] not_interested =		new byte[]{0,0,0,1,3};
 
 	static private ByteBuffer have;
 	static private ByteBuffer bitfield;
@@ -16,14 +16,16 @@ public class MessageLibrary
 	static private ByteBuffer cancel;
 //	private ByteBuffer port;
 	
-	public static byte[] getHaveMessage(int piece_index) {
+	public static byte[] getHaveMessage(int piece_index)
+	{
 		// Remember to adjust the allocate size if you change any of the parameter types
 		have = ByteBuffer.allocate(9);
 		have.put(new byte[]{0,0,0,5,4}); // <length> = 5, <id> = 4
 		have.putInt(piece_index); // <piece index>
 		return have.array();
 	}
-	public static byte[] getBitfieldMessage(byte[] bitfield_parameter) {
+	public static byte[] getBitfieldMessage(byte[] bitfield_parameter)
+	{
 		// Remember to adjust the allocate size if you change any of the parameter types
 		bitfield = ByteBuffer.allocate(5 + bitfield_parameter.length);
 		bitfield.putInt(1 + bitfield_parameter.length); // <length> = 5 + bitfield's length
@@ -31,7 +33,8 @@ public class MessageLibrary
 		bitfield.put(bitfield_parameter); // <bitfield>
 		return bitfield.array();
 	}
-	public static byte[] getRequestMessage(int index, int begin, int length) {
+	public static byte[] getRequestMessage(int index, int begin, int length)
+	{
 		// Remember to adjust the allocate size if you change any of the parameter types
 		request = ByteBuffer.allocate(17);
 		request.put(new byte[]{0,0,0,13,6}); // <length = 13>, <id> = 6
@@ -41,7 +44,8 @@ public class MessageLibrary
 		return request.array();
 	}
 	// Not done yet
-	public static byte[] getPieceMessage(int index, int begin, int block_length, byte[] block) {
+	public static byte[] getPieceMessage(int index, int begin, int block_length, byte[] block)
+	{
 		// Remember to adjust the allocate size if you change any of the parameter types
 		piece = ByteBuffer.allocate(13 + block_length);
 		piece.putInt(9 + block_length); // <length> = 9 + block_length
@@ -52,7 +56,8 @@ public class MessageLibrary
 		return piece.array();
 	}
 	// Not done yet
-	public byte[] getCancelMessage(int index, int begin, int length) {
+	public static byte[] getCancelMessage(int index, int begin, int length)
+	{
 		// Remember to adjust the allocate size if you change any of the parameter types
 		cancel = ByteBuffer.allocate(17);
 		cancel.put(new byte[]{0,0,0,13,8}); // <length> = 13, <id> = 8
