90096de0f3b907a70840de88927ede3dff18ed3b
Will.I.Rall <Will.I.Rall@47b1fde3-503f-0410-ba13-9bbf17867248>
2007-12-10 10:05:53 +0000

    Added a verbose mode (still slightly broken, though). Made things actually work.  Fixed bugs in: piece reception (array index out of bounds exception thrown every time), choking/unchoking (array index out of bounds exception thrown every time, plus logical errors, sorting errors), etc. Now, the BitTortoise will actually receive a piece, though I've not tested beyond it talking to the point where it receives the piece message (have not gotten it to parse yet).

--- 417BitTorrentClient/topThreeComparator.java
+++ 417BitTorrentClient/topThreeComparator.java
@@ -1,15 +1,39 @@
 import java.util.Comparator;
 
 
-public class topThreeComparator implements Comparator<Peer> {
-
-	public int compare(Peer p1, Peer p2) {
-		if(p1.bytesReadThisRound > p2.bytesReadThisRound){
+public class topThreeComparator implements Comparator<Peer>
+{
+	/**
+	 * Compares p1 to p2
+	 * @param p1 the first peer
+	 * @param p2 the second peer
+	 * @return -1 iff p1 < p2, 0 iff p1 == p2, 1 if p1 > p2
+	 */
+	public int compare(Peer p1, Peer p2)
+	{
+		// Note, we want the highest bytesReadThisRound to be at the lowest end of the array (0)
+		if(p1.bytesReadThisRound < p2.bytesReadThisRound)
+		{
 			return 1;
-		}else if(p1.bytesReadThisRound > p2.bytesReadThisRound){
+		}
+		else if(p1.bytesReadThisRound > p2.bytesReadThisRound)
+		{
 			return -1;
-		}else{
-			return 0;
+		}
+		else
+		{
+			if(!p1.am_choking && p2.am_choking)
+			{
+				return -1;
+			}
+			else if(p1.am_choking && !p2.am_choking)
+			{
+				return 1;
+			}
+			else
+			{
+				return 0;
+			}
 		}
 	}
 
