078d7828c9c2c0ae2c1d13b1307beea0081d06fc
Will.I.Rall <Will.I.Rall@47b1fde3-503f-0410-ba13-9bbf17867248>
2007-12-13 20:40:52 +0000

    Changes made to allow resumer to work when it's done.

---
+++ trunk/417BitTorrentClient/Resumer.java
@@ -0,0 +1,108 @@
+import java.util.*;
+import java.util.regex.*;
+import java.io.*;
+
+public class Resumer
+{
+	public static boolean resumeFromStopped(String resumeInfoFilename, RandomAccessFile destinationFile, TorrentFile torrentFile, Map<Integer, Piece> map, BitSet completed, BitSet inProgress, int totalPieces)
+	{
+		completed.set(0, totalPieces, true);
+		inProgress.set(0, totalPieces, false);
+		
+		// Load all of the unfinished block requests from the file:
+		try
+		{
+			RandomAccessFile resumeFile = new RandomAccessFile(resumeInfoFilename, "rw");
+			
+			if(resumeFile.length() == 0)
+				return false;
+			
+			String s;
+			while((s = resumeFile.readLine()) != null)
+			{
+				Piece p;
+				BlockRequest br = BlockRequest.fromString(s);
+				
+				if(br == null)
+					return false;
+				
+				// Destroy other state information:
+				if(br.status != BlockRequest.FINISHED)
+					br.status = BlockRequest.UNASSIGNED;
+				
+				if(map.containsKey(br.piece))
+				{
+					p = map.get(br.piece);
+				}
+				else
+				{
+					p = new Piece(br.piece);
+					map.put(br.piece, p);
+				}
+				p.blocks.add(br);
+				
+				if(br.status != BlockRequest.UNASSIGNED)
+					inProgress.set(br.piece, true);
+				
+				completed.set(br.piece, false);
+			}
+		}
+		catch(IOException e)
+		{
+			return false;
+		}
+		
+		// Load all of the finished pieces (ones not in the map) from the destination file, check their hashes:
+		try
+		{
+			if(destinationFile.length() != torrentFile.file_length)
+				return false;
+			
+			// Cycle through completed pieces, check that their SHA1 hashes are equal:
+			int i = 0;
+			while((i = completed.nextSetBit(i)) != -1)
+			{
+				
+			}
+		}
+		catch(IOException e)
+		{
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public static boolean saveStatus(String resumeInfoFilename, Map<Integer, Piece> map)
+	{
+		String endOfLine = System.getProperty("line.separator");
+		try
+		{
+			RandomAccessFile resumeFile = new RandomAccessFile(resumeInfoFilename, "rw");
+			
+			if(resumeFile.length() > 0)
+				return false;
+			
+			for(Map.Entry<Integer, Piece> entry : map.entrySet())
+			{
+				Piece piece = (Piece)entry.getValue();
+				for(BlockRequest br : piece.blocks)
+				{
+					resumeFile.writeChars(br.toPrintString() + endOfLine);
+				}
+			}
+		}
+		catch(IOException e)
+		{
+			return false;
+		}
+		return true;
+	}
+	
+	public static boolean checkSeed(RandomAccessFile sourceFile, TorrentFile torrentFile)
+	{
+		// Load all of the pieces from the source file, check their hashes:
+		
+		return false;
+	}
+}
